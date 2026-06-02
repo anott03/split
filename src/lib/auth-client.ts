a35637a5bd8bde3864670d6258c08fab29c8b458
Amitav Nott <58123848+anott03@users.noreply.github.com>
2026-06-01 20:53:15 -0500

    auth fixes

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,7 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient()
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+
+const { env } = getCloudflareContext();
+export const authClient = createAuthClient({
+    baseURL: env.BETTER_AUTH_URL,
+})
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,9 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
 import * as schema from "@/lib/schema";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 
+const { env } = getCloudflareContext();
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "sqlite",
@@ -10,5 +12,9 @@ export const auth = betterAuth({
 	}),
     emailAndPassword: {
         enabled: true,
-    }
+    },
+    trustedOrigins: [
+        "http://localhost:3000",
+        env.BETTER_AUTH_URL,
+    ],
 });
